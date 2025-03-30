3d96d774fdb518ebce9e1e688b372949ff8dace9
psanant1208 <psanant1208@gmail.com>
2025-03-30 14:14:22 +0200

    Delete ej21.js

--- ej21.js
+++
@@ -1,4 +0,0 @@
-//21. Utilizando el método substring imprimir las letras que van desde la
-//posición 3 a la 7 de la cadena «wonderful day», es decir “derf”.
-let cadena = "wonderfull day";
-console.log(cadena.substring(3,7));
